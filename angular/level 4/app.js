3c04d29ff8375610d8e5708254b3847fa41702e1
Chris Demeke <demeke2@gmail.com>
2015-06-26 10:20:52 -0400

    Complete level 4

--- angular/level 4/app.js
+++ angular/level 4/app.js
@@ -37,6 +37,27 @@
 		};
 	});
 
+	app.directive('productPanels', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'product-panels.html',
+			controller: function(){
+				this.tab = 1;
+
+				this.selectTab = function(setTab){
+					this.tab = setTab;
+					console.log("hi");
+				};
+
+				this.isSelected = function(checkTab){
+					return this.tab == checkTab;
+				};
+			},
+			controlerAs: 'panels'
+
+		};
+	});
+
 	var product = [
 		{
 			name: 'Shirt 1',
