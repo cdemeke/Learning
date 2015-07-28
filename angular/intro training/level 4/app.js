4ea9464782450a87f094d64887f34fb62cc8cb19
Chris Demeke <demeke2@gmail.com>
2015-07-27 22:05:51 -0400

    created folder for AngularJS tuturial

---
+++ angular/intro training/level 4/app.js
@@ -0,0 +1,99 @@
+(function(){
+	var app = angular.module('store', []);
+
+	app.controller('StoreController', function(){
+		this.products = product;
+
+	});
+
+
+	app.controller('ReviewController', function(){
+		this.review = {};
+
+		this.addReview = function(product){
+			product.reviews.push(this.review);
+			this.review = {};
+		};
+
+	});
+
+	app.directive('productTitle', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'product-title.html'
+		};
+	});
+
+	app.directive('productPanels', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'product-panels.html',
+			controller: function(){
+				this.tab = 1;
+
+				this.selectTab = function(setTab){
+					this.tab = setTab;
+				};
+
+				this.isSelected = function(checkTab){
+					return this.tab == checkTab;
+				};
+
+			},
+			controllerAs: 'panels'
+
+		};
+	});
+
+	var product = [
+		{
+			name: 'Shirt 1',
+			price: 2.43,
+			description: '....',
+			images: [
+				{
+					full: 'http://www.overclock.net/gallery/data/500/homer.png',
+					thumb: 'http://vignette1.wikia.nocookie.net/simpsons/images/2/20/Homer-simpson.png'
+				},
+				{
+					full: 'http://www.overclock.net/gallery/data/500/homer.png',
+					thumb: 'http://vignette1.wikia.nocookie.net/simpsons/images/2/20/Homer-simpson.png'
+				}
+				],
+			reviews: [
+				{
+					stars: 5,
+					body: "I love this product",
+					author: "chris@google.com"
+				}
+				]
+		},
+		{
+			name: 'Shirt 2',
+			price: 3,
+			description: '....',
+			images: [
+				{
+					full: 'http://www.overclock.net/gallery/data/500/homer.png',
+					thumb: 'http://vignette1.wikia.nocookie.net/simpsons/images/2/20/Homer-simpson.png'
+				},
+				{
+					full: 'http://www.overclock.net/gallery/data/500/homer.png',
+					thumb: 'http://vignette1.wikia.nocookie.net/simpsons/images/2/20/Homer-simpson.png'
+				}
+				],
+			reviews: [
+				{
+					stars: 5,
+					body: "I love this product",
+					author: "chris@google.com"
+				}
+				]
+		
+
+		}
+	];
+
+
+
+})();
